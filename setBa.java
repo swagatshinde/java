126e3bd2375a6cf54c4ecab1067409a74ef583ba
swagatshinde <htcswagatshinde@gmail.com>
2026-01-12 09:18:35 +0530

    Update setBa.java

--- setBa.java
+++ setBa.java
@@ -1,7 +1,7 @@
  // Q.Accept 'n' integer from the user.store and display integer in sorted order having proper collection class.
 // The collection should not accept duplicate elements.
 // created treeset object.
-// treeset sort value automatically.
+// treeset sort value automatically and remove duplicates.
 // Purpose of add() method: Adds a single value to a group.
 // the for-each loop visits every single element in the collection from start to finish.
 
@@ -12,7 +12,6 @@ import java.util.TreeSet;
 public class setBa {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // TreeSet automatically sorts elements and removes duplicates
         TreeSet<Integer> ts = new TreeSet<>();
 
         System.out.print("How many values: ");
@@ -25,7 +24,6 @@ public class setBa {
         }
 
         System.out.println("The sorted values are: ");
-        // Fixed: Removed semicolon and printing 'num' instead of 'ts'
         for (Integer num : ts) {
             System.out.println(num);
         }
@@ -34,3 +32,4 @@ public class setBa {
     }
 
 }
+
