2c86951cbc787ee880ab63ef4c32310fc944216d
swagatshinde <htcswagatshinde@gmail.com>
2026-01-12 09:06:59 +0530

    Update setAa.java

--- setAa.java
+++ setAa.java
@@ -1,6 +1,6 @@
 // Q.write a java program to accept names of 'n' cities,insert same into array list collection and display contents of same array list,also remove all these elements
 // created arraylist object.
-// // Purpose of add() method: Adds a single value to a group.
+// Purpose of add() method: Adds a single value to a group.
 // using clear method all elements are remove.
   
 import java.util.*;
@@ -25,5 +25,6 @@ al.clear();
 System.out.println("The cities Are clear : "+al);
 }
 }
+
 
 
