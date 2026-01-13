114c28267d2bcb1334b617b1c6f211bb621ef4e4
swagatshinde <htcswagatshinde@gmail.com>
2026-01-13 08:15:10 +0530

    Add files via upload

---
+++ Slip4_1.java
@@ -0,0 +1,48 @@
+package Slip1;
+
+import java.util.Scanner;
+
+public class Slip4_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter no of rows ");
+		int m=sc.nextInt();
+		System.out.println("enter no of columns");
+		int n=sc.nextInt();
+		
+		int input[][]=new int [m][n];	
+		System.out.println("enter matrix");
+		for(int row=0; row<m; row++)
+		{
+			for(int col=0; col<n; col++)
+			{
+				input[row][col]=sc.nextInt();
+				
+						
+			}
+		}
+		System.out.println("You enterd matrix");
+		for(int row=0; row<m; row++)
+		{
+			for(int col=0; col<n; col++)
+			{
+				System.out.print(input[row][col]+"\t");
+			}
+			System.out.println("");
+		}
+		System.out.println("matrix in transpose");
+		for(int row=0; row<m; row++)
+		{
+			for(int col=0; col<n; col++)
+			{
+				System.out.print(input[col][row]+"\t");
+				
+						
+			}
+			System.out.println();
+		}
+
+	}
+
+}
