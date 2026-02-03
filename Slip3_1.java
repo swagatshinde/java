3d419d981867fe5ea528aba01bed073f0a79fb28
Swagat Shinde <htcswagatshinde@gmail.com>
2026-02-03 18:08:25 +0530

    Add files via upload

---
+++ Slip3_1.java
@@ -0,0 +1,27 @@
+package Slip1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Slip3_1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter n");
+		int n=sc.nextInt();
+		String city []=new String[n];
+		System.out.println("enter "+n+"cities");
+		for(int i=0; i<n; i++)
+		{
+			city[i]=sc.next();
+		}
+		Arrays.sort(city);
+		System.out.println("cities in sorted order");
+		for(String a:city)
+			System.out.println(a);
+		// TODO Auto-generated method stub
+
+	}
+
+}
