74c782641636df275db3283bf647a8cac1d3a190
Swagat Shinde <htcswagatshinde@gmail.com>
2026-03-12 20:34:30 +0530

    Create MultithradingDemo.java

---
+++ MultithradingDemo.java
@@ -0,0 +1,13 @@
+
+public class MultithradingDemo extends Thread 
+{
+	public void run()
+	{
+		System.out.println("My thread is running thread");
+	}
+	public static void main(String[] args) 
+	{
+		MultithradingDemo obj=new MultithradingDemo();
+		obj.start();
+	}
+}
