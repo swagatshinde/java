461629eb42708d69e05ad1c7da92e347473ecb3d
Swagat Shinde <htcswagatshinde@gmail.com>
2026-03-16 20:54:11 +0530

    Create TreeSetDemo.java

---
+++ TreeSetDemo.java
@@ -0,0 +1,27 @@
+
+import java.util.Set;
+import java.util.TreeSet;
+
+class TreeSetDemo{
+	public static void main(String args[])
+	{
+		Set ts=new TreeSet();
+		ts.add("one");
+		ts.add("four");
+		ts.add("three");
+		ts.add("two");
+		ts.add("five");
+		System.out.println("Members From TreeSet"+ts);
+		
+		Set ts1=new TreeSet();
+		ts1.add(3);
+		ts1.add(2);
+		ts1.add(5);
+		ts1.add(1);
+		ts1.add(4);
+		System.out.println("Members From TreeSet"+ts1);
+		
+		
+		
+	}
+}
